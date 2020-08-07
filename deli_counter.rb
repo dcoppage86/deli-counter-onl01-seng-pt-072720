5b5fa68150622b5305be03b6383dc2ca86f05431
Dakota Coppage <dakotakcoppage@me.com>
2020-08-07 03:20:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  place = 1
+  line_array = []
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each do |name|
+    line_array.push("#. #")
+    place += 1
+  end
+    puts "The line is currently: #{line_array.join(" ")"
+  end
+end
+ 
