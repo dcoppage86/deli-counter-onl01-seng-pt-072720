b78a6b875d68a1012bfec1cdc462202213b5a6ae
Dakota Coppage <dakotakcoppage@me.com>
2020-08-07 04:20:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     katz_deli.each do |name|
-    line_array.push("#{place} #{name}")
+    line_array.push("#{place}#{name}")
     place +=1
   end
     puts "The line is currently: #{line_array.join("")}"
