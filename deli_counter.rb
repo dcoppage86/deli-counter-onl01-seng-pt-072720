ac3dc571e7d41a12be2655ae64ea77d1839f091d
Dakota Coppage <dakotakcoppage@me.com>
2020-08-07 04:13:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,7 +6,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.each do |place, name|
+    katz_deli.each do |name|
     line_array.push("#{place}. #{name}")
     place +=1
   end
