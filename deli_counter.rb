5b777f3ee1b26096c6561580b6b54dbd0b355f63
Dakota Coppage <dakotakcoppage@me.com>
2020-08-07 03:41:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@ katz_deli = ["Ada", "Grace", "Kent"]
 
 def line(katz_deli)
   place = 1
-  line_array = ["Ada", "Grace", "Kent"]
+  line_array = []
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
