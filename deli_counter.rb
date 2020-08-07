5a53efb1224300db3212a49a9b0ef277be9de68c
Dakota Coppage <dakotakcoppage@me.com>
2020-08-07 03:26:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  if katz_deli.push.empty?
+  if katz_deli.push(name)
     puts "Welcome, #{name}. You are number #{number} in line."
   end
 end
